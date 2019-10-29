39e11f1f8f59bd8f67d176defa9fac092b934684
Sowmen Rahman <sowmen.appd@gmail.com>
2019-10-30 05:29:51 +0600

    jump rotational torque added

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -12,10 +12,16 @@ public class Bird : MonoBehaviour
     private const float m_jumpEnableThresholdTime = .25f;
     private float m_jumpTimer;
 
+    private float m_lastKnownHeight;
+
+    private float m_rotation;
+    [SerializeField] private float m_torque;
+
     void Start()
     {
         m_verticalVelocity = 0;
         m_jumpTimer = 0;
+        m_rotation = 0;
     }
 
     void Update()
@@ -32,5 +38,11 @@ public class Bird : MonoBehaviour
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.9f, 3.9f), 0);
     
         m_jumpTimer += Time.deltaTime;
+
+        m_rotation += ((transform.position.y > m_lastKnownHeight) ? 3 : -3) * Time.deltaTime * m_torque;
+        m_rotation = Mathf.Clamp(m_rotation, -30, 30);
+        print((transform.position.y > m_lastKnownHeight) ? "ascending" : "descending");
+        transform.rotation = Quaternion.Euler(0, 0, m_rotation);
+        m_lastKnownHeight = transform.position.y;
     }
 }
